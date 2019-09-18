d615848b481d129d5b88ae4aabb70fbeca67df62
Juan J Chorro Bergillo <jjchorrobe@gmail.com>
2019-09-18 09:18:10 +0200

    Add HTTPConnectionConfigException

---
+++ src/main/java/net/galgus/flink/streaming/connectors/http/exception/HTTPConnectionConfigException.java
@@ -0,0 +1,7 @@
+package net.galgus.flink.streaming.connectors.http.exception;
+
+public class HTTPConnectionConfigException extends Exception {
+    public HTTPConnectionConfigException(String msg) {
+        super(msg);
+    }
+}
